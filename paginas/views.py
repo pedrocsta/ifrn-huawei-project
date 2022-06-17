c17e23ab1b2173f0063084475d6d027b0e158e45
Pedro Costa <pedrohdcosta@outlook.com>
2022-06-17 13:25:01 -0300

    Página contact terminada (não responsiva)

--- paginas/views.py
+++ paginas/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'paginas/index.html')
+
+def contact(request):
+    return render(request, 'paginas/contact.html')
