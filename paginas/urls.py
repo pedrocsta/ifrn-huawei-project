69428d6fd743147bcbd5b1936b59b0e9813415cf
Pedro Costa <pedrohdcosta@outlook.com>
2022-06-17 21:43:59 -0300

    Adicionando as páginas

--- paginas/urls.py
+++ paginas/urls.py
@@ -3,5 +3,17 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # SOBRE
+    # 
+
+    # CURSOS
+    # 
+
+    # EVENTOS
+    # 
+    
+    path('laboratories/', views.laboratories, name='laboratories'),
+    path('instructors/', views.instructors, name='instructors'),
+    path('certification/', views.certification, name='certification'),
     path('contact/', views.contact, name='contact'),
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -3,5 +3,23 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'paginas/index.html')
 
+# SOBRE
+# 
+
+# CURSOS
+# 
+
+# EVENTOS
+# 
+
+def laboratories(request):
+    return render(request, 'paginas/laboratories.html')
+
+def instructors(request):
+    return render(request, 'paginas/instructors.html')
+
+def certification(request):
+    return render(request, 'paginas/certification.html')
+
 def contact(request):
     return render(request, 'paginas/contact.html')
